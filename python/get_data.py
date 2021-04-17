6528ba5b22aee1f3a84f5de76b3a65164b7a7b0d
Simon Chervenak <sarras305@gmail.com>
2021-04-17 01:42:13 -0400

    this model kinda works

--- python/get_data.py
+++ python/get_data.py
@@ -9,17 +9,8 @@ mp_hands = mp.solutions.hands
 dist = lambda a, b: math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2 + (a.z - b.z)**2)
 dist_arr = lambda a, b: abs(a - b)
 
-letters = list(map(ast.literal_eval, open('letters.txt').readlines()))
-#letters = [[] for _ in range(26)]
-#for i in range(1, 8):
-#  for a, j in enumerate(map(ast.literal_eval, open(f'../asl_letters/asl_letters_{i}.txt').readlines())):
-#    pass
-#  print(i, a )
-#    # letters[a].append(j)
-#input()
-#letters = list(map(statistics.mean, letters))
-
-dists = lambda h: [dist(h.landmark[i], h.landmark[j]) for (i, j) in mp_hands.HAND_CONNECTIONS]
+# letters = list(map(ast.literal_eval, open('letters.txt').readlines()))
+# dists = lambda h: [dist(h.landmark[i], h.landmark[j]) for (i, j) in mp_hands.HAND_CONNECTIONS]
 
 def classify(dists):
   fn = lambda i: sum(it.starmap(dist_arr, zip(dists, i)))
@@ -33,13 +24,17 @@ def read_static():
       max_num_hands=2,
       min_detection_confidence=0.5) as hands:
 
-    letters = []
-    for (parent, _, files) in tqdm.tqdm(os.walk('../ASL Alphabet Dataset')):
-      if parent == 'ASL Alphabet Dataset':
+    letters2 = [] #[0 for _ in range(26)]
+    for (parent, _, files) in tqdm.tqdm(list(sorted(os.walk('../ASL Alphabet Dataset'), key = lambda i: i[0][-1]))):
+      if parent == '../ASL Alphabet Dataset':
         continue
 
       l = []
 
+      # print(parent, string.ascii_uppercase.index(parent[-1]))
+
+      # for fp in [[i for i in files if i.endswith('.png')][0]]:
+      # fp = max([i for i in files if i.endswith('.png')], key = lambda i: int(i.split("_")[1].split(".")[0]))
       for fp in [i for i in files if i.endswith('.png')]:
         image = cv2.flip(cv2.imread(parent + '/' + fp), 1)
         results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
@@ -48,19 +43,26 @@ def read_static():
           continue
         
         hand_1 = results.multi_hand_landmarks[0]
-        dists = []
-        for (i, j) in mp_hands.HAND_CONNECTIONS:
-          dists.append(dist(hand_1.landmark[i], hand_1.landmark[j]))
-        l.append(dists)
+        l.append(dists(hand_1))
+
+      # annotated_image = image.copy()
+      # mp_drawing.draw_landmarks(
+      #   annotated_image, hand_1, mp_hands.HAND_CONNECTIONS)
+      # cv2.imwrite(
+      # './annotated_image.png', cv2.flip(annotated_image, 1))
+      # input()
+        
       l = list(map(statistics.mean, zip(*l)))
-      letters.append(l)
+      # letters2[string.ascii_uppercase.index(parent[-1])] = l
+      letters2.append(l)
 
   with open("letters.txt", "w") as f:
-    for i in letters[1:]:
+    for i in letters2:
       f.write(str(i))
       f.write('\n')
 
-# read_static()
+read_static()
+letters = list(map(ast.literal_eval, open('letters.txt').readlines()))
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
@@ -91,6 +93,6 @@ with mp_hands.Hands(
             image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
         print(classify(dists(hand_landmarks)))
     cv2.imshow('MediaPipe Hands', image)
-    if cv2.waitKey(0) & 0xFF == 27:
+    if cv2.waitKey(5) & 0xFF == 27:
       break
 cap.release()
